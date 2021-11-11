9c940436a93218ca9bc85294afe61b4d88d933c2
Valentin <valentin.trouillet@modis.com>
2021-11-11 17:27:18 +0100

    added fix

--- src/js/main.js
+++ src/js/main.js
@@ -15,16 +15,17 @@ const removeForm = () => {
  * Queries books using google books API searching with author and title as parameters
  */
 const queryBooks = async() => {
-    //TODO: Dans le cas où la recherche ne retourne pas de résultats, le message“ Aucun
-    // livre n’ a été trouvé” devra être affichee
     console.log('Entering queryBooks');
     //Stops the page from reloading
     event.preventDefault();
     console.log('Title : ' + document.getElementById('bookTitle').value);
     console.log('Author : ' + document.getElementById('bookAuthor').value);
     //Query
-    const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=intitle:\"' + document.getElementById('bookTitle').value + '\" inauthor:\"' + document.getElementById('bookAuthor').value);
+    const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=intitle:\"' + document.getElementById('bookTitle').value.normalize("NFD").replace(/\p{Diacritic}/gu, "") + '\" inauthor:\"' + document.getElementById('bookAuthor').value.normalize("NFD").replace(/\p{Diacritic}/gu, ""));
     const queryResponse = await response.json();
+    if (queryResponse.totalItems == 0) {
+        alert("Aucun livre n’ a été trouvé");
+    }
     let items = queryResponse.items;
     console.log('Reponse complete ');
     document.getElementById('js-search-results').style.display = "inline";
@@ -43,30 +44,57 @@ const queryBooks = async() => {
 }
 
 const addBookToList = (e) => {
-        // Le même livre ne pourra pas être ajouté deux foisdans la poch’ liste.Le
-        // message“ Vous ne pouvez ajouter deux fois le mêmelivre” devra être a
+       
+        //Check if book is already in the list and if so diplay error message
+
         let library = '';
         if (sessionStorage.getItem("pochlist")) {
             library = sessionStorage.getItem("pochlist");
         }
 
-        //TODO: Have a sesisonstorage item library with the keys to the book. then each book is an element
-        //duplicata - verifier id
+        //TODO : duplicata - verifier id
         console.log('storage before' + library);
         let element = e.parentNode.parentNode.outerHTML;
         console.log(element);
+        //Get id of the book
         let idString = element.match(/Id : \w+<\/div>/g).toString().match(/\w{4,}/);
+        if (library.includes(idString)) {
+            alert("Vous ne pouvez ajouter deux fois le même livre");
+            return;
+        }
         console.log('idString ' + idString);
         if (library == '') {
             library = idString;
         } else {
             library = library + ',' + idString;
         }
+
+        // Replace the bookmark icon with a remove icon
+        //  Replace the addBookToList function with the removeBookFromList function
+         element = element.replace('class="fas fa-bookmark"', 'class="fas fa-trash"')
+                          .replace('addBookToList', 'removeBookFromList');
         sessionStorage.setItem("pochlist", library);
         sessionStorage.setItem(idString, element);
         console.log('storage after' + sessionStorage.getItem("pochlist"));
         displayPochList();
     }
+
+    /**
+     * Removes a book from the pochlist
+     * @param {*} e Element to remove from the list 
+     */
+const removeBookFromList = (e) => {
+    let library = sessionStorage.getItem("pochlist");
+    let idString = e.parentNode.parentNode.outerHTML.match(/Id : \w+<\/div>/g).toString().match(/\w{4,}/);
+    library = library.replace(idString, '');
+    sessionStorage.setItem("pochlist", library);
+    sessionStorage.removeItem(idString);
+    displayPochList();
+}
+
+
+
+
     /**
      * Displays pochlist if there are books stored in the session
      */
@@ -88,9 +116,8 @@ const displayPochList = () => {
         console.log('bookThumbnail ' + bookThumbnail);
         let book = document.createElement("div");
         book.innerHTML = bookThumbnail;
-        div.appendChild(book);
+        src.appendChild(book);
     });
-    src.appendChild(div);
     }
 }
 
@@ -121,9 +148,8 @@ class HandleList {
         title.className = "card-section h4";
         title.innerText = "Titre : " + this.element.volumeInfo.title;
         card.appendChild(title);
-        let flag = document.createElement("button");
-        flag.className = '';
-        flag.innerHTML = 'Add to list';
+        let flag = document.createElement("span");
+        flag.className ="fas fa-bookmark";
         flag.setAttribute("onclick", "addBookToList(this)");
         // flag.addEventListener("click", addBookToList());
         card.appendChild(flag);
@@ -138,14 +164,18 @@ class HandleList {
         card.appendChild(authors);
         let description = document.createElement("div");
         description.className = "card-section h4";
+        if(this.element.volumeInfo.description)
         description.innerText = "Description : " + this.truncateString(this.element.volumeInfo.description, 100);
+        else
+        description.innerText = "Description : Aucune description disponible";
         card.appendChild(description);
         let img = document.createElement("img");
         img.className = 'book-cover';
         //If there is no thumbnail available, use the default image
         if (this.element.volumeInfo.imageLinks) {
             //HQ Cover but less availability
-            img.src = this.element.volumeInfo.imageLinks.thumbnail.replace('&zoom=1&', '&zoom=0&');
+            img.src = this.element.volumeInfo.imageLinks.thumbnail;
+            // img.src = this.element.volumeInfo.imageLinks.thumbnail.replace('&zoom=1&', '&zoom=0&');
         } else img.src = "src/img/unavailable.png";
         //Append the img to the div and return it
         div.appendChild(card);
